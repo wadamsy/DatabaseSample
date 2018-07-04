03e9b09b1545098792f3efdb9e3e0c869537d10a
wadamasaya <16010418@anabuki-college.ac.jp>
2018-07-04 11:29:33 +0900

    init

---
+++ app/src/main/java/wadamasaya/databasesample/CocktailMemoActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.databasesample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CocktailMemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cocktail_memo);
+    }
+}
